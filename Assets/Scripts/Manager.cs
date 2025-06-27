515dd7fc9fdb3e12224996f799456801480ab206
byf <1765613885@qq.com>
2025-06-28 00:42:32 +0800

    add skillbase and objectbase

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Manager : MonoSingleton<Manager>
 {
     public MusicConfig MusicConfig;
+    public List<ObjectBase> Object;
+
+    //将作祟物体加入Object管理
+    void AddObject(ObjectBase go)
+    {
+        this.Object.Add(go);
+    }
 }
---
+++ Assets/Scripts/SkillBase.cs
@@ -0,0 +1,6 @@
+
+//@TODO:完善技能系统
+public class SkillBase
+{
+
+}
